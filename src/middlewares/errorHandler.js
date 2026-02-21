ce72c21817b260c04177d88089c20dfe5c97942f
FVS-MBA <feliven@gmail.com>
2026-02-21 13:09:19 -0300

    trata erros de validação e adiciona mensagens de erro personalizadas

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,6 +3,14 @@ import mongoose from "mongoose";
 function errorHandler(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
     res.status(400).send({ message: "Dados fornecidos estão incorretos" });
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    console.log(erro.errors);
+
+    const mensagensErro = Object.values(erro.errors)
+      .map((erro) => erro.message)
+      .join("; ");
+
+    res.status(400).send({ message: `Ocorreram estes erros de validação de dados: ${mensagensErro}` });
   } else {
     res.status(500).send({ message: "Erro interno do servidor" });
   }
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema({
   id: { type: String },
-  titulo: { type: String, required: true },
-  autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true },
-  editora: { type: String, required: true },
+  titulo: { type: String, required: [true, "Título é obrigatório"] },
+  autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "Autor é obrigatório"] },
+  editora: { type: String, required: [true, "Nome da editora é obrigatório"] },
   numeroPaginas: { type: Number },
 });
 

02167e91a39246a0c6a963b18646a3f6ff9582eb
FVS-MBA <feliven@gmail.com>
2026-02-21 21:55:59 -0300

    melhora validador global com trim()

--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose.Schema.Types.String.set("validate", {
-  validator: (valor) => valor !== "",
+  validator: (valor) => valor.trim() !== "",
   message: ({ path }) => `Campo '${path}' em branco não é permitido`,
 });

5c25a7f778bc2345199f0af1c75b82490ee79134
FVS-MBA <feliven@gmail.com>
2026-02-28 23:21:05 -0300

    adiciona variáveis campoOrdenacao e ordem à busca de livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -5,16 +5,17 @@ import { autores, livros } from "../models/index.js";
 class LivroController {
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, campoOrdenacao = "_id", ordem = -1 } = req.query;
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livrosResultado = await livros
           .find()
           .collation({ locale: "pt", strength: 2 }) // strength 2 ignores case
-          .sort({ editora: 1 })
+          .sort({ [campoOrdenacao]: ordem })
           .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor")
